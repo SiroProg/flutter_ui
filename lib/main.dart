c88fe7815f2e52aac7bc2a3b8aa83b5af775be19
SiroProg <070777sss@gmail.com>
2023-08-15 23:04:22 +0500

    UI_5

--- lib/main.dart
+++ lib/main.dart
@@ -4,123 +4,98 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Home Work",
       home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Container(
-            width: 300,
-            height: 300,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 255, 17, 65),
-                  Color.fromARGB(255, 50, 7, 111),
-                ],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: const Text(
+                "Select Your Avatar",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            child: Stack(
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: const CircleAvatar(
+                radius: 75,
+                backgroundImage: NetworkImage(
+                  "https://pbs.twimg.com/media/EmJM7q2WoAEbbLD.png",
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Center(
-                  child: Container(
-                    width: 250,
-                    height: 250,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromARGB(255, 255, 17, 65),
-                          Color.fromARGB(255, 50, 7, 111),
-                        ],
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 220, left: 100),
-                    child: Container(
-                      padding: EdgeInsets.all(4),
-                      margin: EdgeInsets.only(left: 180, top: 200),
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 255, 17, 65),
-                            Color.fromARGB(255, 50, 7, 111),
-                          ],
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                        ),
-                      ),
-                      child: Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 2,
-                          ),
-                        ),
-                        child: Stack(
-                          children: [
-                            Center(
-                              child: Text(
-                                '20',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(top: 16, left: 37),
-                              width: 7,
-                              height: 7,
-                              padding: EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 1,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 200, left: 300),
-                    child: Text(
-                      '#051E22',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 500, left: 100),
-                    child: Text(
-                      '#051E22',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                )
+                build(
+                    "https://avatarko.ru/img/kartinka/2/zhivotnye_Novyj_god_krysa_1742.jpg"),
+                build("https://pbs.twimg.com/media/EmJMR0XWkAAjhNr.png"),
               ],
             ),
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                build(
+                    "https://i.pinimg.com/736x/91/bb/57/91bb5782fadf21713330c978670f8c44.jpg"),
+                build("https://avatarko.ru/img/kartinka/1/krysa_Remi.jpg"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                build(
+                    "https://avatarko.ru/img/kartinka/9/zhivotnye_krysa_8908.jpg"),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    radius: 50,
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    backgroundColor: Colors.grey[200],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10, top: 20),
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Center(
+                  child: Text(
+                'Select',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              )),
+            ),
+          ],
         ),
       ),
+    ),
+  );
+}
+
+Widget build(String imgUrl) {
+  return Container(
+    margin: const EdgeInsets.all(10),
+    child: CircleAvatar(
+      radius: 50,
+      backgroundImage: NetworkImage(imgUrl),
     ),
   );
 }
